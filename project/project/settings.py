cf076bfac06e7b88c374e114484f080d732fb70b
OttoAndrey <30920379+OttoAndrey@users.noreply.github.com>
2020-12-22 23:41:59 +0700

    Connect postgres

--- project/project/settings.py
+++ project/project/settings.py
@@ -83,10 +83,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.dj_db_url('DATABASE_URL', 'sqlite:///db.sqlite3')
 }
 
 
